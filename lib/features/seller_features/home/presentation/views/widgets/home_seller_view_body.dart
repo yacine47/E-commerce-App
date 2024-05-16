35339a626987e984f8245c42c1086dc6671c5f10
yacine47 <iyac2077@gmail.com>
2024-05-16 07:50:03 +0100

    add icon dashbord info to items

--- lib/features/seller_features/home/presentation/views/widgets/home_seller_view_body.dart
+++ lib/features/seller_features/home/presentation/views/widgets/home_seller_view_body.dart
@@ -1,8 +1,7 @@
 import 'package:ecommerce_app/constants.dart';
-import 'package:ecommerce_app/core/utils/my_colors.dart';
-import 'package:ecommerce_app/core/utils/styles.dart';
 import 'package:ecommerce_app/core/widgets/custom_app_bar.dart';
 import 'package:ecommerce_app/core/widgets/item_has_padding.dart';
+import 'package:ecommerce_app/features/seller_features/home/presentation/views/widgets/seller_dashbord_item.dart';
 import 'package:flutter/material.dart';
 import 'package:iconly/iconly.dart';
 
@@ -11,10 +10,10 @@ class HomeSellerViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView(
+    return const CustomScrollView(
       // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       slivers: [
-        const SliverToBoxAdapter(
+        SliverToBoxAdapter(
           child: Column(
             children: [
               SizedBox(height: 23),
@@ -27,52 +26,31 @@ class HomeSellerViewBody extends StatelessWidget {
           ),
         ),
         SliverPadding(
-          padding: const EdgeInsets.symmetric(horizontal: kHorPadding),
-          sliver: SliverGrid.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          padding: EdgeInsets.symmetric(horizontal: kHorPadding),
+          sliver: SliverGrid(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 2,
               crossAxisSpacing: 10,
               mainAxisSpacing: 16,
               childAspectRatio: 2 / 2.5,
             ),
-            itemCount: 3,
-            itemBuilder: (context, index) => Container(
-              decoration: const BoxDecoration(boxShadow: [
-                BoxShadow(
-                    blurRadius: 20,
-                    color: Color.fromARGB(15, 0, 0, 0),
-                    offset: Offset(2, 4),
-                    blurStyle: BlurStyle.normal),
-              ]),
-              child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
-                ),
-                elevation: 0,
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Icon(
-                      IconlyLight.bag_2,
-                      size: 42,
-                    ),
-                    const SizedBox(height: 8),
-                    Text(
-                      '15',
-                      style: Styles.style24,
-                    ),
-                    Text(
-                      'Sales',
-                      style: Styles.style20.copyWith(
-                        color: MyColors.hintColorTextField,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    )
-                  ],
-                ),
+            delegate: SliverChildListDelegate.fixed([
+              SellerDashbordItem(
+                count: 15,
+                icon: Icons.local_mall_outlined,
+                title: 'Sales',
               ),
-            ),
+              SellerDashbordItem(
+                count: 15,
+                icon: Icons.inventory_2_outlined,
+                title: 'Sales',
+              ),
+              SellerDashbordItem(
+                count: 15,
+                icon: Icons.description_outlined,
+                title: 'Sales',
+              ),
+            ]),
           ),
         )
 
